2652d975595ee52001000cc7e7987e2c15735c05
Salvatore Nalbone <salvatorenalbone.web@gmail.com>
2017-10-10 12:24:14 +0200

    Update DB Seeder

--- src/Installs/seeds/DatabaseSeeder.php
+++ src/Installs/seeds/DatabaseSeeder.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Code generated using LaraAdmin
- * Help: http://laraadmin.com
- * LaraAdmin is open-sourced software licensed under the MIT license.
- * Developed by: Dwij IT Solutions
- * Developer Website: http://dwijitsolutions.com
- */
 
 use Illuminate\Database\Seeder;
 
@@ -21,150 +14,159 @@ use App\Models\Department;
 
 class DatabaseSeeder extends Seeder
 {
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		
-		/* ================ LaraAdmin Seeder Code ================ */
-		
-		// Generating Module Menus
-		$modules = Module::all();
-		$teamMenu = Menu::create([
-			"name" => "Team",
-			"url" => "#",
-			"icon" => "fa-group",
-			"type" => 'custom',
-			"parent" => 0,
-			"hierarchy" => 1
-		]);
-		foreach ($modules as $module) {
-			$parent = 0;
-			if($module->name != "Backups") {
-				if(in_array($module->name, ["Users", "Departments", "Employees", "Roles", "Permissions"])) {
-					$parent = $teamMenu->id;
-				}
-				Menu::create([
-					"name" => $module->name,
-					"url" => $module->name_db,
-					"icon" => $module->fa_icon,
-					"type" => 'module',
-					"parent" => $parent
-				]);
-			}
-		}
-		
-		// Create Administration Department
-	   	$dept = new Department;
-		$dept->name = "Administration";
-		$dept->tags = "[]";
-		$dept->color = "#000";
-		$dept->save();
-		
-		// Create Super Admin Role
-		$role = new Role;
-		$role->name = "SUPER_ADMIN";
-		$role->display_name = "Super Admin";
-		$role->description = "Full Access Role";
-		$role->parent = 1;
-		$role->dept = $dept->id;
-		$role->save();
-		
-		// Set Full Access For Super Admin Role
-		foreach ($modules as $module) {
-			Module::setDefaultRoleAccess($module->id, $role->id, "full");
-		}
-		
-		// Create Admin Panel Permission
-		$perm = new Permission;
-		$perm->name = "ADMIN_PANEL";
-		$perm->display_name = "Admin Panel";
-		$perm->description = "Admin Panel Permission";
-		$perm->save();
-		
-		$role->attachPermission($perm);
-		
-		// Generate LaraAdmin Default Configurations
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sitename";
-		$laconfig->value = "LaraAdmin 1.0";
-		$laconfig->save();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
 
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sitename_part1";
-		$laconfig->value = "Lara";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sitename_part2";
-		$laconfig->value = "Admin 1.0";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sitename_short";
-		$laconfig->value = "LA";
-		$laconfig->save();
+        /* ================ LaraAdmin Seeder Code ================ */
 
-		$laconfig = new LAConfigs;
-		$laconfig->key = "site_description";
-		$laconfig->value = "LaraAdmin is a open-source Laravel Admin Panel for quick-start Admin based applications and boilerplate for CRM or CMS systems.";
-		$laconfig->save();
+        // Generating Module Menus
+        $modules = Module::all();
+        $teamMenu = Menu::firstOrCreate([
+            "name" => "File Manager",
+            "url" => "file-manager",
+            "icon" => "fa-folder-open",
+            "type" => 'custom',
+            "parent" => 0,
+            "hierarchy" => 0
+        ]);
+        $teamMenu = Menu::firstOrCreate([
+            "name" => "Team",
+            "url" => "#",
+            "icon" => "fa-group",
+            "type" => 'custom',
+            "parent" => 0,
+            "hierarchy" => 1
+        ]);
+        foreach ($modules as $module) {
+            $parent = 0;
+            if($module->name != "Backups" && $module->name != "Uploads") {
+                if(in_array($module->name, ["Users", "Departments", "Employees", "Roles", "Permissions"])) {
+                    $parent = $teamMenu->id;
+                }
+                Menu::firstOrCreate([
+                    "name" => $module->name,
+                    "url" => $module->name_db,
+                    "icon" => $module->fa_icon,
+                    "type" => 'module',
+                    "parent" => $parent
+                ]);
+            }
+        }
 
-		// Display Configurations
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sidebar_search";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "show_messages";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "show_notifications";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "show_tasks";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "show_rightsidebar";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "skin";
-		$laconfig->value = "skin-white";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "layout";
-		$laconfig->value = "fixed";
-		$laconfig->save();
+        // Create Administration Department
+        $dept = Department::firstOrNew(["name" => "Administration"]);
+        $dept->tags = "[]";
+        $dept->color = "#000";
+        if ($dept->id == null) {
+            $dept->save();
+        }
+        // Create Super Admin Role
+        $role = Role::firstOrNew(["name" => "SUPER_ADMIN"]);
+        $role->display_name = "Super Admin";
+        $role->description = "Full Access Role";
+        $role->parent = 1;
+        $role->dept = $dept->id;
+        if ($role->id == null) {
+            $role->save();
+        }
 
-		// Admin Configurations
+        // Set Full Access For Super Admin Role
+        foreach ($modules as $module) {
+            Module::setDefaultRoleAccess($module->id, $role->id, "full");
+        }
 
-		$laconfig = new LAConfigs;
-		$laconfig->key = "default_email";
-		$laconfig->value = "test@example.com";
-		$laconfig->save();
-		
-		$modules = Module::all();
-		foreach ($modules as $module) {
-			$module->is_gen=true;
-			$module->save();	
-		}
+        // Create Admin Panel Permission
+        $perm = Permission::firstOrNew(["name" => "ADMIN_PANEL"]);
+        $perm->display_name = "Admin Panel";
+        $perm->description = "Admin Panel Permission";
+        if ($perm->id == null) {
+            $perm->save();
+        }
+        if (! $role->perms->contains($perm->id)) {
+            $role->attachPermission($perm);
+        }
 
-		/* ================ Call Other Seeders ================ */
-		
-	}
+        // Generate LaraAdmin Default Configurations
+
+        $laconfig = LAConfigs::firstOrNew(["key" => "sitename"]);
+        $laconfig->value = "AdminBase 1.0";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+
+        $laconfig = LAConfigs::firstOrNew(["key" => "sitename_part1"]);
+        $laconfig->value = "Admin";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "sitename_part2"]);
+        $laconfig->value = "Base 1.0";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "sitename_short"]);
+        $laconfig->value = "AB";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "site_description"]);
+        $laconfig->value = "Admin Base is a base solution to have a working base admin panel with integrated CRUD booth for API and WebApp, automatic API documentation";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        // Display Configurations
+
+        $laconfig = LAConfigs::firstOrNew(["key" => "sidebar_search"]);
+        $laconfig->value = "1";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "show_messages"]);
+        $laconfig->value = "1";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "show_notifications"]);
+        $laconfig->value = "1";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "show_tasks"]);
+        $laconfig->value = "1";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "show_rightsidebar"]);
+        $laconfig->value = "1";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "skin"]);
+        $laconfig->value = "skin-green";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $laconfig = LAConfigs::firstOrNew(["key" => "layout"]);
+        $laconfig->value = "fixed";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        // Admin Configurations
+
+        $laconfig = LAConfigs::firstOrNew(["key" => "default_email"]);
+        $laconfig->value = "test@example.com";
+        if ($laconfig->id == null) {
+            $laconfig->save();
+        }
+        $modules = Module::all();
+        foreach ($modules as $module) {
+            $module->is_gen=true;
+            $module->save();
+        }
+    }
 }
